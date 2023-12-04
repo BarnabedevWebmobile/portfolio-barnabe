3083227fc38b937ea2b8ef32cc6682b70c2358cc
BarnabedevWebmobile <barnabe.dwwm48@gmail.com>
2023-12-04 10:29:35 +0100

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -35,7 +35,7 @@
 <section class="project"> <!--l'ensemble des cartes-->
 <div class="container">
     <div class="row justify-content-center animate__animated animate__lightSpeedInLeft"> 
-        <div class="col-10 col-md-4 card p-3 m-3 carte"><!--la carte-->
+        <div class="col-10 col-md-3 card p-3 m-3 carte"><!--la carte-->
             <img src="<?php echo bloginfo('template_directory' )."/image/projet1.png"?>" class="card-img-top p-3 img-fluid" alt="onepage"> <!--l'image de la carte-->
             <div class="card-body p-3"> <!--le contenu de la carte-->
               <h5 class="card-title">Mon premier projet</h5>
@@ -47,7 +47,7 @@
               <a href="https://github.com/BarnabedevWebmobile/fiche-biographie-arthur-morgan" class="btncentercard btn btn-primary">github</a>
             </div>
         </div>
-        <div class="col-10 col-md-4 card p-3 m-3 carte "><!--la carte-->
+        <div class="col-10 col-md-3 card p-3 m-3 carte "><!--la carte-->
             <img src="<?php echo bloginfo('template_directory' )."/image/projet2.png"?>" class="card-img-top p-3 img-fluid" alt="memory"><!--l'image de la carte-->
             <div class="card-body p-3"><!--le contenu de la carte-->
               <h5 class="card-title">projet en JavaScript</h5>
@@ -59,7 +59,7 @@
               <a href="https://github.com/BarnabedevWebmobile/memory" class="btncentercard btn btn-primary">github</a>
             </div>
         </div>
-        <div class="col-10 col-md-4 card p-3 m-3 carte "> <!--la carte-->
+        <div class="col-10 col-md-3 card p-3 m-3 carte "> <!--la carte-->
             <img src="<?php echo bloginfo('template_directory' )."/image/projet3.png"?>" class="card-img-top p-3 img-fluid" alt="portfolio"> <!--l'image de la carte-->
             <div class="card-body p-3"><!--le contenu de la carte-->
               <h5 class="card-title">Mon portfolio</h5>
