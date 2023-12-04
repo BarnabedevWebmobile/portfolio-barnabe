6df0d5cc9ea4542dd59c83483ff3ceae2065e3f4
BarnabedevWebmobile <barnabe.dwwm48@gmail.com>
2023-12-04 09:23:07 +0100

    404

--- 404.php
+++ 404.php
@@ -2,5 +2,5 @@
     <?php get_header(); ?>
     <h1 style="color : #000"> 404 not found</h1>
         <h2><?php bloginfo( 'template_directory' ); ?></h2>
-        <img src="http://localhost/wordpress/wp-content/uploads/2023/11/1677514858_c75fac44cc0fbe5d489f56b1dcab6d74.jpg" alt="erreur 404">
+        <img src="<?php echo bloginfo('template_directory' )."/image/404.avif"?>" alt="erreur 404">
         <?php wp_footer(); ?>
